5dd17a029643b5909d445ed1f6cf4baa99bab9df
Daniel Neset <danielneset@gmail.com>
2025-03-16 20:20:35 +0100

    Initial spring boot gateway commit

---
+++ src/main/java/no/ntnu/gr10/bachelor_gateway/BachelorGatewayApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.gr10.bachelor_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachelorGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/ntnu/gr10/bachelor_gateway/BachelorGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.gr10.bachelor_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachelorGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
