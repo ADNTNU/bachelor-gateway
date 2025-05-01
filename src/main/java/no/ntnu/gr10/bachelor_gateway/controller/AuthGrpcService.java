8e0765011fb88e77b510c866706d005152092902
Daniel Neset <danielneset@gmail.com>
2025-05-01 21:32:32 +0200

    Change so when authenticating it returns the token, company id and its scopes. Also added reflection for ease of client use

--- src/main/java/no/ntnu/gr10/bachelor_gateway/controller/AuthGrpcService.java
+++ src/main/java/no/ntnu/gr10/bachelor_gateway/controller/AuthGrpcService.java
@@ -13,16 +13,18 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 @GrpcService
-public class AuthGrcpService extends AuthGrpc.AuthImplBase {
+public class AuthGrpcService extends AuthGrpc.AuthImplBase {
   private final AuthenticationManager authManager;
   private final JwtUtil jwtUtil;
 
   @Autowired
-  public AuthGrcpService(AuthenticationManager authManager, JwtUtil jwtUtil) {
+  public AuthGrpcService(AuthenticationManager authManager, JwtUtil jwtUtil) {
     this.authManager = authManager;
     this.jwtUtil = jwtUtil;
   }
@@ -37,9 +39,11 @@ public class AuthGrcpService extends AuthGrpc.AuthImplBase {
 
       CustomUserDetails user = (CustomUserDetails) auth.getPrincipal();
 
-      // TODO refactor
+      System.out.println(user.getCompanyId());
+      System.out.println(user.getAuthorities());
+
       Map<String,Object> claims = Map.of(
-              "companyId",     user.getCompanyId(),
+              "companyId", user.getCompanyId(),
               "scopes", user.getAuthorities()
                       .stream()
                       .map(a -> Map.of("authority", a.getAuthority()))
@@ -48,13 +52,14 @@ public class AuthGrcpService extends AuthGrpc.AuthImplBase {
 
       String token = jwtUtil.generateToken(user.getUsername(), claims);
 
-      resp.onNext(
-              AuthProto.AuthResponse.newBuilder()
-                      .setToken(token)
-                      .setUsername(user.getUsername())
-                      .setCompanyId(user.getCompanyId())
-                      .build()
-      );
+      AuthProto.AuthResponse out = AuthProto.AuthResponse.newBuilder()
+              .setToken(token)
+              .setCompanyId(user.getCompanyId())
+              .addAllRoles(user.getAuthorities().stream()
+                      .map(GrantedAuthority::getAuthority)
+                      .toList())
+              .build();
+      resp.onNext(out);
       resp.onCompleted();
 
     } catch (BadCredentialsException e) {
